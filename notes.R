3b1fd89727e2d8748d72038ee9db2fc3db64d10d
Ntt37Vp <fuckspammers@protonmail.com>
2023-09-02 02:21:17 +0800

    more day 1 notes

---
+++ notes.R
@@ -0,0 +1,30 @@
+#More Notes
+
+# Data Types. Check using class() func; ex:
+x <- 10.5
+class(x)
+
+# integer
+y <- 1000L
+class(x)
+
+# complex
+z <- 9i + 3
+class(x)
+
+# character/string
+a <- "R is exciting"
+class(x)
+
+# logical/boolean
+b <- TRUE
+class(x) 
+
+# There are three number types in R:
+# Numeric - most common type in R, and contains any number with or without a decimal, like: 10.5, 55, 787
+
+# Integer - numeric data without decimals. To create an integer variable, you must use the letter L after the integer value:
+k <- 1000L
+
+# Complex
+
