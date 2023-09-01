1bc530dac43d0d602ccae967423e8c77b1822220
Ntt37Vp <fuckspammers@protonmail.com>
2023-09-01 22:31:01 +0800

    initial commit

---
+++ hello.r
@@ -0,0 +1,14 @@
+print("Hello World of R!")
+# I am a comment :)
+
+# Variables
+
+# Assignment operator <-
+first_var <- "I am a variable"
+second_var <- 12.5
+
+# Vector using c()
+vec_1 <- c(1, 2, 3, 5, 8, 13)
+
+
+# Pipe %>%
