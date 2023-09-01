73b1e8dad8811850477303404a5c7c9fa82121c2
Ntt37Vp <fuckspammers@protonmail.com>
2023-09-01 22:57:30 +0800

    day 1 notes

--- hello.r
+++ hello.r
@@ -12,3 +12,12 @@ vec_1 <- c(1, 2, 3, 5, 8, 13)
 
 
 # Pipe %>%
+
+# Arithmetic using regular + - * /
+
+quarter_1_sales <- 300000
+quarter_2_sales <- 600000
+midyear_sales <- quarter_1_sales + quarter_2_sales
+projected_year_end_sales <- midyear_sales * 2
+
+print(projected_year_end_sales)
