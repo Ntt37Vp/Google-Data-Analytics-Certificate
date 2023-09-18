3a08ee64fc108815f6899e7f01eec2f44897327a
Ntt37Vp <fuckspammers@protonmail.com>
2023-09-18 19:05:56 +0800

    vizuals using ggplot2

--- week-4-notes.r
+++ week-4-notes.r
@@ -5,3 +5,17 @@
 # Ploty
 # Lattice
 # RGL
+
+# Hands On - 1
+# install.packages("ggplot2")
+# install.packages("palmerpenguins")
+# library(ggplot2)
+# library(palmerpenguins)
+# data("penguins")
+# View(penguins)
+# ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm, y=body_mass_g))
+# ggplot(data=penguins)
+
+
+
+# Getting Started with ggplot()
