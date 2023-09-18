5dcef5ce26b5ba27a062cdae2a49798e992fae21
Ntt37Vp <fuckspammers@protonmail.com>
2023-09-19 00:47:56 +0800

    geom_bar and smoothing

--- week-4-notes.r
+++ week-4-notes.r
@@ -19,3 +19,24 @@
 
 
 # Getting Started with ggplot()
+# install, # library then call the func
+# format: ggplot(data=) + geom_point(mapping=aes())
+# library(ggplot2)
+# library(palmerpenguins)
+# data("penguins")
+# View(penguins)
+# ggplot(data = penguins) +
+#     geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+# ggplot(data = penguins) +
+#     geom_point(mapping = aes(x = bill_length_mm, y = bill_depth_mm, color = species))
+# ggplot(data = penguins) +
+#     geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species, shape = species, alpha = species))
+# ggplot(data = penguins) +
+#     geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species, shape = species, alpha = species), color = "purple")
+
+# using GEOM_SMOOTH
+# ggplot(data=penguins) + geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g))
+
+# ggplot(data = diamonds) + geom_bar(mapping = aes(x=cut))
+# ggplot(data = diamonds) + geom_bar(mapping = aes(x=cut, color=cut))
+# ggplot(data = diamonds) + geom_bar(mapping = aes(x=cut, fill=cut))
