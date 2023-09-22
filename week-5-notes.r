88614def322fcebeb0873336fe870f7d07c62332
Ntt37Vp <fuckspammers@protonmail.com>
2023-09-23 00:03:58 +0800

    RMD notes and "sample_rmd.rmd" file

--- week-5-notes.r
+++ week-5-notes.r
@@ -2,3 +2,18 @@
 
 ## RMD - R Markdown
 # install.packages("rmarkdown")
+
+
+## Sample RMD on separate "sample_rmd.rmd"
+
+# # Adding Bullets use asterisk
+
+# * Python
+# * R
+# * Go
+
+# # Adding links using []()
+# [Wikipedia](https://wikipedia.org)
+
+# # Embedding images using ![]()
+# ![Rick Roll](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.WjuBvVAXEMBWElXcxRkx0AHaFf%26pid%3DApi&f=1&ipt=0da67860d35c3e7e25c018f138922bc38c8c86ab7ec4dc4c5c1f3163209331b2&ipo=images)
